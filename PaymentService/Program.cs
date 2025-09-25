688daebe1f0ca684f45cf1da31326f3ddaacfb2f
max-henr <maxipod.henriksson@gmail.com>
2025-09-25 13:07:29 +0200

    bug fix

--- PaymentService/Program.cs
+++ PaymentService/Program.cs
@@ -8,22 +8,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 builder.Configuration.AddAzureKeyVault(new Uri("https://group-project-keyvault.vault.azure.net/"), new DefaultAzureCredential());
 
-var stripeSecretKey = builder.Configuration["StripeSecretKey"];
 
 
 var app = builder.Build();
 
-
+var cfg = app.Services.GetRequiredService<IConfiguration>();
+var stripeSecretKey = cfg["StripeSecretKey"];
 Stripe.StripeConfiguration.ApiKey = stripeSecretKey;
 
 
+app.UseHttpsRedirection();
 app.MapControllers();
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentService API");
+    c.RoutePrefix = string.Empty;
+});
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseAuthentication();
+app.UseAuthorization();
 
-
-app.Urls.Add("https://group-project-paymentservice-eyh8h2ewfqhvgddc.swedencentral-01.azurewebsites.net/");
 
 app.Run();
