93b0460d54ad2d611a8f6ead9eedce93ce527d39
max-henr <maxipod.henriksson@gmail.com>
2025-09-25 12:26:40 +0200

    Add controller for checkout, azure keys, general structure, controller for webhooks

--- PaymentService/Program.cs
+++ PaymentService/Program.cs
@@ -1,23 +1,29 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Stripe;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Configuration.AddAzureKeyVault(new Uri("https://group-project-keyvault.vault.azure.net/"), new DefaultAzureCredential());
+
+var stripeSecretKey = builder.Configuration["StripeSecretKey"];
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
-app.UseHttpsRedirection();
+Stripe.StripeConfiguration.ApiKey = stripeSecretKey;
 
-app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+app.Urls.Add("https://group-project-paymentservice-eyh8h2ewfqhvgddc.swedencentral-01.azurewebsites.net/");
 
 app.Run();
